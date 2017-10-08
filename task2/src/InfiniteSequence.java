465b34aa7becbae7b00d23d37b299222269362b2
Gravechapa <Gravechapa@protonmail.com>
2017-10-08 06:36:26 +0300

    optimization

--- task2/src/InfiniteSequence.java
+++ task2/src/InfiniteSequence.java
@@ -1,38 +1,122 @@
 public class InfiniteSequence
 {
-    public int findSequence(int sequence)
+    private String _sequence;
+    private int _sequenceLength;
+
+    public long findSequence(String sequence)
     {
-        if (sequence == 0)
+        _sequence = sequence;
+        _sequenceLength = sequence.length();
+        long result = -1;
+
+        for (int power = 1; power <= _sequenceLength; ++power)
             {
-                return 11;
+                for (int offset = 0; offset < power; ++offset)
+                    {
+                        long infOffset = findCoincidences(power, offset);
+                        if (infOffset != -1 && (result == -1 || result > infOffset))
+                            {
+                                result = infOffset;
+                            }
+                    }
+
+                if (result != -1)
+                    {
+                        break;
+                    }
             }
-        if (sequence <=9 && sequence >= 1)
-            {
-                return sequence;
-            }
-        int result;
-        String seq = Integer.toString(sequence);
-        String infSeq = "";
-        int counter = 1;
-        int offset = 1;
-        while (true)
-        {
-            while (infSeq.length() < seq.length() * 4)
-                {
-                    infSeq += counter++;
-                }
-            result = infSeq.indexOf(seq);
 
-            if (result != -1)
-                {
-                    result += offset;
-                    break;
-                }
-
-            offset += infSeq.length() - seq.length() + 1;
-            infSeq = infSeq.substring(infSeq.length() - seq.length() + 1);
-
-        }
         return result;
+    }
+
+    private long findCoincidences(int power, int offset)
+        {
+            String temp = null;
+            Long value = -1L;
+            Long lastValue;
+            Long startValue = -1L;
+            int currentPosition = offset;
+
+            Long maxValue = (long)Math.pow(10, power) - 1;
+
+            while (currentPosition < _sequenceLength)
+                {
+                    lastValue = value;
+
+                    if (currentPosition + power <= _sequenceLength)
+                        {
+                            temp = _sequence.substring(currentPosition, currentPosition + power);
+                            if (temp.charAt(0) == '0')
+                                {
+                                    return -1L;
+                                }
+                            value = Long.valueOf(temp);
+                        }
+                    else
+                        {
+                            int overflow = currentPosition + power - _sequenceLength;
+                            String temp2 = _sequence.substring(currentPosition, currentPosition + power - overflow);
+                            if (temp2.charAt(0) == '0')
+                                {
+                                    return -1L;
+                                }
+
+                            if (currentPosition > offset)
+                                {
+                                    temp = temp.substring(temp.length() - overflow, temp.length());
+                                    value = nextValue(Long.valueOf(temp2), Long.valueOf(temp), temp.length());
+                                }
+                            else
+                                {
+                                    temp = _sequence.substring(offset - overflow, offset);
+                                    value = nextValue(Long.valueOf(temp2), Long.valueOf(temp), temp.length());
+                                }
+                        }
+
+                    if (currentPosition == offset)
+                        {
+                            startValue = value;
+                            if (offset > 0)
+                                {
+                                    String offsetSequence = _sequence.substring(0, offset);
+                                    String comparedString = String.valueOf(value - 1);
+                                    comparedString = comparedString.substring(comparedString.length() - offset);
+                                    if (!comparedString.equals(offsetSequence))
+                                        {
+                                            return -1L;
+                                        }
+                                }
+                        }
+                    else if(value - lastValue != 1)
+                        {
+                            return -1;
+                        }
+
+                    currentPosition += power;
+                    if (value.equals(maxValue))
+                        {
+                            ++power;
+                            maxValue += 9L * (long) Math.pow(10, power);
+                        }
+
+                }
+
+            long result = 0;
+            int startValuePower = startValue.toString().length();
+            for (int i = 1; i < startValuePower; ++i)
+                {
+                    result += 9 * (long)Math.pow(10, i - 1) * i;
+                }
+            startValue -= (long)Math.pow(10, startValuePower - 1);
+            result += startValue * startValuePower + 1;
+            return result - offset;
+        }
+
+    private long nextValue(Long value, Long lastNumbers, int power)
+    {
+        long powerOfTen = (long)Math.pow(10, power);
+        return lastNumbers < powerOfTen - 1 ?
+                value * powerOfTen + lastNumbers + 1 :
+                value * powerOfTen;
     }
 }
